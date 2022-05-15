17630c7a1aaed6f88f49043b0f043330bf00ad8e
spinfast319 <spinfast319@protonmail.com>
2022-05-15 00:19:54 -0400

    added error for windows bad characters

--- Update-Origin.py
+++ Update-Origin.py
@@ -14,6 +14,7 @@ import shutil # Imports functionality that lets you copy files and directory
 import datetime # Imports functionality that lets you make timestamps
 import subprocess  # Imports functionality that let's you run command line commands in a script
 import requests # Imports the ability to make web or api requests
+import re # Imports regex
 
 
 #  Set your directories here
@@ -30,6 +31,9 @@ headers = {"Authorization": api_key}
 # Set up the counters for completed albums and missing origin files
 count = 0
 missing = 0
+bad_folder_name = 0
+album_missing = 0
+link_missing = 0
 
 #intro text
 print("")
@@ -37,85 +41,103 @@ print("Engage!")
 print("")
 
 
+
+
 #  A function that gets the url of the album, downloads a new origin script and replaces the old one
 def update_origin(directory):
         global count
         global missing
+        global bad_folder_name
+        global album_missing
+        global link_missing
         global headers
+        print ("\n")
         #check to see if folder has bad characters and skip if it does
-        print("Getting new origin file for " + directory)
-        #check to see if there is an origin file
-        file_exists = os.path.exists('origin.yaml')
-        #if origin file exists, load it, get url, run gazelle-origin, download new one, replace old one
-        if file_exists == True:
-            #open the yaml and turn the data into variables
-            with open(directory + '\origin.yaml',encoding='utf-8') as f:
-              data = yaml.load(f, Loader=yaml.FullLoader)
-            album_url = data['Permalink']    
-                        
-            # check to see if there is a gazelle link that exists and works
-            if album_url != None:
-                
-                # check to see if the url in origin fils goes to a real album
-                # get the torrent id from the permalink
-                torrent_id = album_url.split("=")
-                torrent_id = torrent_id[-1]
-                # create the ajax page
-                ajax_page = "https://redacted.ch/ajax.php?action=torrent&id="
-                ajax_page = ajax_page + torrent_id           
-                # do an api request to get back a success or failure
-                r = requests.get(ajax_page, headers=headers)
-                status = r.json()
-                if status['status'] == "success":
-                    print ("--The album was located.")            
-                    print("--The album is at " + album_url)
-                    
-                    # run gazelle origin on the url and write the new origin file to the directory
-                    the_command = "gazelle-origin -t " + site_ident + " --api-key " + api_key + " " + album_url + " -o \"" + directory + "\\neworigin.yaml\"" #windows
-                    print("--Downloading origin file as neworigin.yaml")
-                    subprocess.run (the_command) # Executes the gazzelle origin command on the directory you are in
-            
-                    #delete the origin file
-                    #print (directory + "\\origin.yaml")
-                    os.remove(directory + "\\origin.yaml")
-            
-                    #rename neworigin to origin
-                    print("--Renamed neworigin.yaml to origin.yaml")
-                    os.rename(directory + "\\neworigin.yaml", directory + "\\origin.yaml")
-            
-                    '''#copy to completed directory
-                    print(directory)
-                    album_folder = os.path.split(directory)
-                    album_folder = album_folder[-1]
-                    new_album_path = completed_directory + album_folder
-                    shutil.copytree(directory, new_album_path)'''
-          
-                    count +=1 # variable will increment every loop iteration
-                else:
-                    print("--The album is no longer on the site.")
-            else:
-                print("--The is origin file is missing a link to the album.")
-        #otherwise log that the origin file is missing
+        #get album name from directory
+        re1 = re.compile(r"[\\/∕:*\"<>|?]");
+        name_to_check = directory.split("\\")
+        name_to_check = name_to_check[-1]
+        if re1.search(name_to_check):
+            print ("Illegal windows character detected.")
+            bad_folder_name +=1 # variable will increment every loop iteration
         else:
-            missing +=1 # variable will increment every loop iteration
-            today = datetime.datetime.now()
-            #split the director to make sure that it distinguishes between foldrs that should and shouldn't have origin files
-            path_segments = directory.split("\\")
-            #create different log files depending on whether the origin file is missing somewhere it shouldn't be
-            if len(path_segments) == 5:
-                #log the missing origin file folders that are likely supposed to be missing
-                print ("--Missing origin file logged.")
-                the_good_log = log_directory + "good-log.txt"
-                with open(the_good_log, 'a') as log_file1:
-                    log_file1.write("{:%b, %d %Y}".format(today)+ " at " +"{:%H:%M:%S}".format(today)+ ".\n")
-                    log_file1.write(directory + "\\ is missing an origin file.\n")
-            else:    
-                #log the missing origin file folders that are not likely supposed to be missing
-                print ("--Missing origin file logged.")
-                the_bad_log = log_directory + "bad-log.txt"
-                with open(the_bad_log, 'a') as log_file2:
-                    log_file2.write("{:%b, %d %Y}".format(today)+ " at " +"{:%H:%M:%S}".format(today)+ ".\n")
-                    log_file2.write(directory + "\\ is missing an origin file.\n")
+            print ("No illegal windows character detected.")
+        
+            print("Getting new origin file for " + directory)
+            #check to see if there is an origin file
+            file_exists = os.path.exists('origin.yaml')
+            #if origin file exists, load it, get url, run gazelle-origin, download new one, replace old one
+            if file_exists == True:
+                #open the yaml and turn the data into variables
+                with open(directory + '\origin.yaml',encoding='utf-8') as f:
+                  data = yaml.load(f, Loader=yaml.FullLoader)
+                album_url = data['Permalink']    
+                            
+                # check to see if there is a gazelle link that exists and works
+                if album_url != None:
+                    
+                    # check to see if the url in origin fils goes to a real album
+                    # get the torrent id from the permalink
+                    torrent_id = album_url.split("=")
+                    torrent_id = torrent_id[-1]
+                    # create the ajax page
+                    ajax_page = "https://redacted.ch/ajax.php?action=torrent&id="
+                    ajax_page = ajax_page + torrent_id           
+                    # do an api request to get back a success or failure
+                    r = requests.get(ajax_page, headers=headers)
+                    status = r.json()
+                    if status['status'] == "success":
+                        print ("--The album was located.")            
+                        print("--The album is at " + album_url)
+                        
+                        # run gazelle origin on the url and write the new origin file to the directory
+                        the_command = "gazelle-origin -t " + site_ident + " --api-key " + api_key + " " + album_url + " -o \"" + directory + "\\neworigin.yaml\"" #windows
+                        print("--Downloading origin file as neworigin.yaml")
+                        subprocess.run (the_command) # Executes the gazzelle origin command on the directory you are in
+                
+                        #delete the origin file
+                        #print (directory + "\\origin.yaml")
+                        os.remove(directory + "\\origin.yaml")
+                
+                        #rename neworigin to origin
+                        print("--Renamed neworigin.yaml to origin.yaml")
+                        os.rename(directory + "\\neworigin.yaml", directory + "\\origin.yaml")
+                
+                        '''#copy to completed directory
+                        print(directory)
+                        album_folder = os.path.split(directory)
+                        album_folder = album_folder[-1]
+                        new_album_path = completed_directory + album_folder
+                        shutil.copytree(directory, new_album_path)'''
+              
+                        count +=1 # variable will increment every loop iteration
+                    else:
+                        print("--The album is no longer on the site.")
+                        album_missing +=1 # variable will increment every loop iteration
+                else:
+                    print("--The is origin file is missing a link to the album.")
+                    link_missing +=1 # variable will increment every loop iteration
+            #otherwise log that the origin file is missing
+            else:
+                missing +=1 # variable will increment every loop iteration
+                today = datetime.datetime.now()
+                #split the director to make sure that it distinguishes between foldrs that should and shouldn't have origin files
+                path_segments = directory.split("\\")
+                #create different log files depending on whether the origin file is missing somewhere it shouldn't be
+                if len(path_segments) == 5:
+                    #log the missing origin file folders that are likely supposed to be missing
+                    print ("--Missing origin file logged.")
+                    the_good_log = log_directory + "good-log.txt"
+                    with open(the_good_log, 'a') as log_file1:
+                        log_file1.write("{:%b, %d %Y}".format(today)+ " at " +"{:%H:%M:%S}".format(today)+ ".\n")
+                        log_file1.write(directory + "\\ is missing an origin file.\n")
+                else:    
+                    #log the missing origin file folders that are not likely supposed to be missing
+                    print ("--Missing origin file logged.")
+                    the_bad_log = log_directory + "bad-log.txt"
+                    with open(the_bad_log, 'a') as log_file2:
+                        log_file2.write("{:%b, %d %Y}".format(today)+ " at " +"{:%H:%M:%S}".format(today)+ ".\n")
+                        log_file2.write(directory + "\\ is missing an origin file.\n")
         
 # Get all the subdirectories of directory_to_update recursively and store them in a list:
 directories = [os.path.abspath(x[0]) for x in os.walk(directory_to_update)]
@@ -129,6 +151,9 @@ for i in directories:
 #summary text
 print("")
 print("Aye Aye Captain. This script updated " + str(count) + " origin files.")
+print("There were " + str(bad_folder_name) + " folders with illegal characters.")
+print("There were " + str(album_missing) + " albums no longer on the site.")
+print("There were " + str(link_missing) + " origin files with a missing link to the album.")
 print("There were " + str(missing) + " folders with no origin files. Check the log to see what they were.")
 
 # ToDo
