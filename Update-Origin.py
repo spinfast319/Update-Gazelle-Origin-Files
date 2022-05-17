9b878f668eab51b6fd212d43cae06a46a26450fb
spinfast319 <spinfast319@protonmail.com>
2022-05-16 23:08:42 -0400

    added improved messaging, added os.sep, tested in linux, cleaned up

--- Update-Origin.py
+++ Update-Origin.py
@@ -5,7 +5,8 @@
 # It takes the folder and opens the yaml to get the url. It then uses the url to run origin query and names the file something differnt.
 # It then deletes the original origin and renames the new one to origin.
 # It can handle albums with artwork folders or multiple disc folders in them. It can also handle specials characters and removes any characters that makes windows fail.
-# You need gazelle-origin installled for it to work.
+# It has been tested and works in both ubuntu linux and windows 10
+# You need a fork of gazelle-origin with the extra metatdata installled for it to work.
 
 # Import dependencies
 import os  # Imports functionality that let's you interact with your operating system
@@ -18,15 +19,21 @@ import re # Imports regex
 
 
 #  Set your directories here
-directory_to_update = "M:\Python Test Environment\Albums\\" # Which directory has the albums you want to update the origin files for?
-completed_directory =  "M:\Python Test Environment\Done\\" # Which directory has the albums you want to update the origin files for?
-log_directory = "M:\Python Test Environment\Logs\\" # Which directory do you want the log albums that have missing origin files in?
-work_directory = "M:\Python Test Environment\Work\\"  # Create directory for downloading the origin file to before you move it to the final directory.
+album_directory = "M:\Python Test Environment\Albums" # Which directory has the albums you want to update the origin files for?
+log_directory = "M:\Python Test Environment\Logs" # Which directory do you want the log albums that have missing origin files in?
+work_directory = "M:\Python Test Environment\Work"  # Create directory for downloading the origin file to before you move it to the final directory.
+
+
+'''#  Set your linux directories here
+album_directory = "/mnt/m/Python Test Environment/Albums" # Which directory has the albums you want to update the origin files for
+log_directory = "/mnt/m/Python Test Environment/Logs" # Which directory do you want the log albums that have missing origin files in?
+work_directory = "/mnt/m/Python Test Environment/Work"  # Create directory for downloading the origin file to before you move it to the final directory.
+'''
 
 # Set your site and API information here
 # for linux you can just have this be in your ~/.bashrc file
 site_ident = "" # set your gazelle site here
-site_ajax_page = ""
+site_ajax_page = "" # set the gazelle ajax page here
 api_key = "" # set your api key here
 headers = {"Authorization": api_key}
 
@@ -42,7 +49,6 @@ error_message = 0
 #intro text
 print("")
 print("Engage!")
-print("")
 
 # A function to log events
 def log_outcomes(d,p,m):
@@ -52,9 +58,9 @@ def log_outcomes(d,p,m):
     log_name = p
     directory = d
     message = m
-    album_name = directory.split("\\")
+    album_name = directory.split(os.sep)
     album_name = album_name[-1]
-    log_path = log_directory + log_name + ".txt"
+    log_path = log_directory + os.sep + log_name + ".txt"
     with open(log_path, 'a',encoding='utf-8') as log_name:
         log_name.write("--{:%b, %d %Y}".format(today)+ " at " +"{:%H:%M:%S}".format(today)+ " from the " + script_name + ".\n")
         log_name.write("The album " + album_name + " " + message + ".\n")
@@ -75,7 +81,7 @@ def update_origin(directory):
         #check to see if folder has bad characters and skip if it does
         #get album name from directory
         re1 = re.compile(r"[\\/:*\"<>|?]");
-        name_to_check = directory.split("\\")
+        name_to_check = directory.split(os.sep)
         name_to_check = name_to_check[-1]
         if re1.search(name_to_check):
             print ("Illegal windows character detected.")
@@ -92,46 +98,38 @@ def update_origin(directory):
             #if origin file exists, load it, get url, run gazelle-origin, download new one, replace old one
             if file_exists == True:
                 #open the yaml and turn the data into variables
-                with open(directory + '\origin.yaml',encoding='utf-8') as f:
+                with open(directory + os.sep + 'origin.yaml',encoding='utf-8') as f:
                   data = yaml.load(f, Loader=yaml.FullLoader)
                 album_url = data['Permalink']
                 clean_directory = data['Directory']   
-                print (directory)
-                #directory = directory_to_update + clean_directory
-                #print (directory)
-                            
+                                            
                 # check to see if there is a gazelle link that exists and works
                 if album_url != None:
                     
-                    # check to see if the url in origin fils goes to a real album
+                    # check to see if the url in origin file goes to a real album
                     # get the torrent id from the permalink
                     torrent_id = album_url.split("=")
                     torrent_id = torrent_id[-1]
                     # create the ajax page
                     ajax_page = site_ajax_page
-                    ajax_page = ajax_page + torrent_id   
-                    print(ajax_page)        
+                    ajax_page = ajax_page + torrent_id          
                     # do an api request to get back a success or failure
                     r = requests.get(ajax_page, headers=headers)
                     status = r.json()
                     if status['status'] == "success":
                         print ("--The album was located.")            
-                        print("--The album is at " + album_url)
-                        the_command = "gazelle-origin -t " + site_ident + " --api-key " + api_key + " " + album_url + " -o \"" + work_directory + "origin.yaml\"" #windows
-                        print("--Downloading origin file as neworigin.yaml")
-                        subprocess.run (the_command) # Executes the gazzelle origin command on the directory you are in
+                        #print("--The album is at " + album_url)
+                        the_command = "gazelle-origin -t " + site_ident + " --api-key " + api_key + " " + album_url + " -o \"" + work_directory + os.sep + "origin.yaml\"" 
+                        print("--Downloading new origin file as origin.yaml in work directory")
+                        subprocess.run (the_command, shell=True) # Executes the gazzelle origin command on the directory you are in
                 
                         #delete the origin file
                         print ("--Removed old origin file.")
-                        os.remove(directory + "\\origin.yaml")
-                
-                        #rename neworigin to origin
-                        #print("--Renamed neworigin.yaml to origin.yaml")
-                        #os.rename(directory + "\\neworigin.yaml", directory + "\\origin.yaml")
+                        os.remove(directory + os.sep + "origin.yaml")              
                         
                         #move origin.yaml file from work to directory
-                        print("--Moved origin.yaml from work directory to " + clean_directory)
-                        os.rename(work_directory + "origin.yaml", directory + "\\origin.yaml")
+                        print("--Moved new origin.yaml from work directory to " + clean_directory)
+                        os.rename(work_directory + os.sep + "origin.yaml", directory + os.sep + "origin.yaml")
                 
                         count +=1 # variable will increment every loop iteration
                     else:
@@ -148,10 +146,11 @@ def update_origin(directory):
                     log_message = "origin file is missing a link to the album"
                     log_outcomes(directory,log_name,log_message)
                     link_missing +=1 # variable will increment every loop iteration
+                    
             #otherwise log that the origin file is missing
             else:
-                #split the director to make sure that it distinguishes between foldrs that should and shouldn't have origin files
-                path_segments = directory.split("\\")
+                #split the directory to make sure that it distinguishes between folders that should and shouldn't have origin files
+                path_segments = directory.split(os.sep)
                 #create different log files depending on whether the origin file is missing somewhere it shouldn't be
                 if len(path_segments) == 5:
                     #log the missing origin file folders that are likely supposed to be missing
@@ -170,16 +169,16 @@ def update_origin(directory):
                     log_outcomes(directory,log_name,log_message)
                     bad_missing +=1 # variable will increment every loop iteration
         
-# Get all the subdirectories of directory_to_update recursively and store them in a list:
-directories = [os.path.abspath(x[0]) for x in os.walk(directory_to_update)]
-directories.remove(os.path.abspath(directory_to_update)) # If you don't want your main directory included
+# Get all the subdirectories of album_directory recursively and store them in a list:
+directories = [os.path.abspath(x[0]) for x in os.walk(album_directory)]
+directories.remove(os.path.abspath(album_directory)) # If you don't want your main directory included
 
-#  Run a loop that goes into each directory identified and updates the origin file
+# Run a loop that goes into each directory identified and updates the origin file
 for i in directories:
       os.chdir(i)         # Change working Directory
       update_origin(i)      # Run your function
 
-#summary text
+# Summary text
 print("")
 print("Aye Aye Captain. This script updated " + str(count) + " origin files.")
 if bad_folder_name >= 1:
@@ -201,9 +200,4 @@ if error_message >= 1:
     print("Check the logs to see which folders had errors and what they were.")
 else:
     print("There were no errors.")    
-
-# ToDo
-# figure out global ajax
-# test in linux
-# --see if i can move the trailing slashes to the directory variable
 
